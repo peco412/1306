0abc79eab964140347798cff1edbdd05f0cb874d
peco412 <phuongdt041201@gmail.com>
2024-12-11 17:38:27 +0700

    first commit

---
+++ 1306.js
@@ -0,0 +1,28 @@
+let items = document.querySelectorAll('.item');
+console.log(items);
+items.forEach(item => {
+    item.addEventListener('mousemove', (e)=>{
+        let positionPx = e.x - item.getBoundingClientRect().left;
+        let positionX = (positionPx / item.offsetWidth) * 100;
+        console.log(positionX, positionPx);
+
+        let positionPy = event.y - item.getBoundingClientRect().top;
+        let positionY = (positionPy / item.offsetHeight) * 100;
+
+        
+        item.style.setProperty('--rX', (0.5)*(50 - positionY) + 'deg');
+        item.style.setProperty('--rY', -(0.5)*(50 - positionX) + 'deg');
+    })
+    item.addEventListener('mouseout', ()=>{
+        item.style.setProperty('--rX', '0deg');
+        item.style.setProperty('--rY', '0deg');
+    })
+})
+
+function playVideo() {
+    const img = document.querySelector('img');
+    const video = document.getElementById('videoElement');
+    img.style.display = 'none';
+    video.style.display = 'block';
+    video.play();
+}
